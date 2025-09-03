684cff261208d06942af2590729ccbe03219315f
Sai Ram <117701637+sairam030@users.noreply.github.com>
2025-09-03 23:07:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title("🥤 Customize Your Smoothie! 🥤")
