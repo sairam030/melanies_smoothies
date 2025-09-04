13be868c9f320d67cad3eb7a86c95e4ab4693382
Sai Ram <117701637+sairam030@users.noreply.github.com>
2025-09-04 15:09:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,25 +47,25 @@ ingredients_list = st.multiselect(
 #             else:
 #                 st.warning(f"Sorry, no data found for {fruit_chosen}")
 
-if ingredients_tist:
+if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'.iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # Add a Submit button
-if st.button("Submit Order"):
-    my_insert_stmt = """
-    INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-    VALUES (?, ?)
-"""
-    session.sql(my_insert_stmt, (ingredients_string, name_on_order)).collect()
-
-
-    st.success('Your Smoothie is ordered! ✅')
+    if st.button("Submit Order"):
+        my_insert_stmt = """
+        INSERT INTO smoothies.public.orders(ingredients, name_on_order)
+        VALUES (?, ?)
+        """
+        session.sql(my_insert_stmt, (ingredients_string, name_on_order)).collect()
+    
+    
+        st.success('Your Smoothie is ordered! ✅')
 
